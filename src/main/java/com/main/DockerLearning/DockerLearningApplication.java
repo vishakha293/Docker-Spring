28c59fecbf079e41dea7b319576d2e902f6943eb
Vedanty5501 <Vedanty5501@gmail.com>
2025-01-02 13:08:24 +0530

    Initial commit

---
+++ src/main/java/com/main/DockerLearning/DockerLearningApplication.java
@@ -0,0 +1,13 @@
+package com.main.DockerLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/DockerLearning/DockerLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.DockerLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
